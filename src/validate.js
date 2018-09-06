b9a4b8f6d754fa0e99135d62960c1d12df34e73e
dhershman1 <dustinh17@gmail.com>
2018-09-05 23:16:16 -0400

    Improved primary validation function, more performant layout

--- src/validate.js
+++ src/validate.js
@@ -1,38 +1,14 @@
+import assign from 'kyanite/assign'
+import every from 'kyanite/every'
+import type from 'kyanite/type'
+
 import validCVN from './cvn'
 import validDate from './expired'
 import validMatch from './matches'
 import validNumber from './number'
 
-// Builds out our nice readable object
-const validation = ({ number, cvn, date }) => {
-  const validationResults = [
-    validNumber(number),
-    validCVN(cvn),
-    validDate(date),
-    validMatch(cvn, number)
-  ]
-  let count = 0
-
-  const results = validationResults.reduce((acc, r) => {
-    const keys = Object.keys(r)
-    const currKey = keys[keys.length - 1]
-
-    if (!r.isValid) {
-      count++
-    }
-
-    acc[currKey] = r[currKey]
-
-    return acc
-  }, {})
-
-  results.isValid = count === 0
-
-  return results
-}
-
 /**
- * @name validate
+ * @name simpleCard
  * @since v2.0.0
  * @category Function
  * @description Validates a credit card object comparing and validating each piece of data on the card passed in
@@ -53,11 +29,18 @@ const validation = ({ number, cvn, date }) => {
   }); // => { isValid: false, cardType: 'visa', cvnType: 'norm', expired: 'Not Expired', info: 'CVN does not match the found card type' }
  */
 const simpleCard = card => {
-  if (Object.prototype.toString.call(card) !== '[object Object]') {
+  if (type(card) !== 'Object') {
     throw new TypeError('Must send full card object to run full validation')
   }
 
-  return validation(card)
+  const validationResults = [
+    validNumber(card.number),
+    validCVN(card.cvn),
+    validDate(card.date),
+    validMatch(card.cvn, card.number)
+  ]
+
+  return assign(...validationResults, { isValid: every(x => x.isValid, validationResults) })
 }
 
 export default simpleCard
