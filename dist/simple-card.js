535a859b2c94b66036e995e2be24430ed6f223de
dhershman1 <dustinh17@gmail.com>
2018-09-05 23:41:12 -0400

    v2.2.0 build files

--- dist/simple-card.js
+++ dist/simple-card.js
@@ -1,69 +1,39 @@
 (function (global, factory) {
-  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports) :
-  typeof define === 'function' && define.amd ? define(['exports'], factory) :
-  (factory((global.simpleCard = {})));
-}(this, (function (exports) { 'use strict';
+  typeof exports === 'object' && typeof module !== 'undefined' ? factory(exports, require('kyanite/or'), require('kyanite/type'), require('kyanite/find'), require('kyanite/range'), require('kyanite/some'), require('kyanite/and'), require('kyanite/not'), require('kyanite/assign'), require('kyanite/every')) :
+  typeof define === 'function' && define.amd ? define(['exports', 'kyanite/or', 'kyanite/type', 'kyanite/find', 'kyanite/range', 'kyanite/some', 'kyanite/and', 'kyanite/not', 'kyanite/assign', 'kyanite/every'], factory) :
+  (factory((global.simpleCard = {}),global.or,global.type,global.find,global.range,global.some,global.and,global.not,global.assign,global.every));
+}(this, (function (exports,or,type,find,range,some,and,not,assign,every) { 'use strict';
+
+  or = or && or.hasOwnProperty('default') ? or['default'] : or;
+  type = type && type.hasOwnProperty('default') ? type['default'] : type;
+  find = find && find.hasOwnProperty('default') ? find['default'] : find;
+  range = range && range.hasOwnProperty('default') ? range['default'] : range;
+  some = some && some.hasOwnProperty('default') ? some['default'] : some;
+  and = and && and.hasOwnProperty('default') ? and['default'] : and;
+  not = not && not.hasOwnProperty('default') ? not['default'] : not;
+  assign = assign && assign.hasOwnProperty('default') ? assign['default'] : assign;
+  every = every && every.hasOwnProperty('default') ? every['default'] : every;
 
   var typeCheck = (function (x) {
-    return typeof x === 'string' || typeof x === 'number';
+    return or(type(x) === 'String', type(x) === 'Number');
   });
 
-  var getCvnType = function getCvnType(cvn) {
-    var len = cvn.length;
-    if (len === 3) {
-      return 'norm';
-    }
-    if (len === 4) {
-      return 'amex';
-    }
-    return false;
-  };
   var validCVN = (function (cvnCode) {
     if (!typeCheck(cvnCode)) {
       throw new TypeError('cvn should be a string or number type');
     }
-    var results = {
-      isValid: false,
-      cvnType: 'Invalid CVN Code'
-    };
-    var sanitizedCVN = String(cvnCode).replace(/\D/g, '');
-    var type = getCvnType(sanitizedCVN);
-    if (type) {
-      return {
-        isValid: true,
-        cvnType: type
-      };
+    var sanitized = String(cvnCode).replace(/\D/g, '');
+    var type$$1 = 'Invalid CVN Code';
+    if (sanitized.length === 3) {
+      type$$1 = 'norm';
+    } else if (sanitized.length === 4) {
+      type$$1 = 'amex';
     }
-    return results;
-  });
-
-  var fullYear = function fullYear(year) {
-    if (year.length === 2) {
-      return "20".concat(year);
-    }
-    return year;
-  };
-  var generateDate = function generateDate() {
-    var dateObj = new Date();
-    return new Date("".concat(dateObj.getMonth() + 1, "/1/").concat(dateObj.getFullYear()));
-  };
-  var normalizeDate = function normalizeDate(date) {
-    var cleanDate = date.replace(/\s/g, '');
-    var splitDate = cleanDate.split(/\W/g);
-    return "".concat(splitDate[0], "/1/").concat(fullYear(splitDate[1]));
-  };
-  var expired = function expired(date) {
-    if (typeof date !== 'string') {
-      throw new TypeError('date should be a string type');
-    }
-    var currDate = generateDate();
-    var expireDate = new Date(normalizeDate(date));
-    var isExpired = !isNaN(expireDate) ? currDate > expireDate : true;
     return {
-      isValid: !isExpired,
-      isExpired: isExpired
+      isValid: type$$1 !== 'Invalid CVN Code',
+      cvnType: type$$1
     };
-  };
+  });
 
   function _slicedToArray(arr, i) {
     return _arrayWithHoles(arr) || _iterableToArrayLimit(arr, i) || _nonIterableRest();
@@ -123,60 +93,61 @@
     throw new TypeError("Invalid attempt to destructure non-iterable instance");
   }
 
-  var range = function range(from, to) {
-    var result = [];
-    var i = from;
-    for (i; i < to; i++) {
-      result.push(i);
+  var fullYear = function fullYear(year) {
+    if (year.length === 2) {
+      return "".concat(String(new Date().getFullYear()).slice(0, 2)).concat(year);
     }
-    return result;
+    return year;
   };
+  var expired = function expired(date) {
+    if (type(date) !== 'String') {
+      throw new TypeError('date should be a string type');
+    }
+    var dateObj = new Date();
+    var _date$replace$split = date.replace(/\s/g, '').split(/\W/g),
+        _date$replace$split2 = _slicedToArray(_date$replace$split, 2),
+        month = _date$replace$split2[0],
+        year = _date$replace$split2[1];
+    var currDate = new Date("".concat(dateObj.getMonth() + 1, "/1/").concat(dateObj.getFullYear()));
+    var expireDate = new Date("".concat(month, "/1/").concat(fullYear(year)));
+    var isExpired = !isNaN(expireDate) ? currDate > expireDate : true;
+    return {
+      isValid: !isExpired,
+      isExpired: isExpired
+    };
+  };
+
   var getCardType = (function (ccNumber) {
     var types = {
       amex: [34, 37],
-      discover: [6011].concat(_toConsumableArray(range(622126, 622925)), _toConsumableArray(range(644, 649)), [65]),
-      master: range(50, 55),
+      discover: [6011].concat(_toConsumableArray(range(622126, 622926)), _toConsumableArray(range(644, 650)), [65]),
+      master: range(50, 56),
       visa: [4]
     };
-    for (var cardType in types) {
-      var _types$cardType$filte = types[cardType].filter(function (n) {
+    return find(function (k) {
+      return some(function (n) {
         return new RegExp("^".concat(n)).test(ccNumber);
-      }),
-          _types$cardType$filte2 = _slicedToArray(_types$cardType$filte, 1),
-          found = _types$cardType$filte2[0];
-      if (found) {
-        return cardType;
-      }
-    }
+      }, types[k]);
+    }, Object.keys(types));
   });
 
-  var invalidAmex = function invalidAmex(cvn, cardType) {
-    return cvn.length === 4 && cardType !== 'amex';
-  };
-  var invalidNorm = function invalidNorm(cvn, cardType) {
-    return cvn.length === 3 && cardType === 'amex';
-  };
-  var match = function match(cvn, type) {
-    if (invalidAmex(cvn, type)) {
-      return false;
-    }
-    if (invalidNorm(cvn, type)) {
-      return false;
-    }
-    return true;
-  };
   var validMatch = (function (cvn, card) {
-    if (!typeCheck(cvn) || !typeCheck(card)) {
+    if (or(!typeCheck(cvn), !typeCheck(card))) {
       throw new TypeError('cvn and card number should be string or number types');
     }
-    var matches = match(String(cvn).replace(/\W/g, ''), getCardType(String(card).replace(/\W/g, '')));
+    var cvnLen = String(cvn).replace(/\D/g, '').length;
+    var cardType = getCardType(String(card).replace(/\D/g, ''));
+    var isValid = not(or(and(cvnLen === 4, cardType !== 'amex'), and(cvnLen === 3, cardType === 'amex')));
     return {
-      isValid: matches,
-      match: !matches ? 'cvn does not match card type' : 'card type matches cvn'
+      isValid: isValid,
+      match: !isValid ? 'cvn does not match card type' : 'card type matches cvn'
     };
   });
 
   var luhnChk = function luhnChk(value) {
+    if (/[^0-9-\s]+/.test(value)) {
+      return false;
+    }
     var numArr = [0, 2, 4, 6, 8, 1, 3, 5, 7, 9];
     var len = value.length;
     var bit = 1;
@@ -184,7 +155,8 @@
     var num = 0;
     while (len) {
       num = parseInt(value.charAt(--len), 10);
-      sum += (bit ^= 1) ? numArr[num] : num;
+      bit ^= 1;
+      sum += bit ? numArr[num] : num;
     }
     return sum && sum % 10 === 0;
   };
@@ -192,37 +164,24 @@
     if (!typeCheck(card)) {
       throw new TypeError('number should be a string or number type');
     }
-    var sanitizedNumber = String(card).replace(/\W/g, '');
-    var valid = Boolean(luhnChk(sanitizedNumber));
+    var sanitized = String(card).replace(/\D/g, '');
+    var isValid = Boolean(luhnChk(sanitized));
     return {
-      isValid: valid,
-      cardType: valid ? getCardType(sanitizedNumber) : 'Invalid Card Number'
+      isValid: isValid,
+      cardType: isValid ? getCardType(sanitized) : 'Invalid Card Number'
     };
   };
 
-  var validation = function validation(_ref) {
-    var number = _ref.number,
-        cvn = _ref.cvn,
-        date = _ref.date;
-    var validationResults = [cNumber(number), validCVN(cvn), expired(date), validMatch(cvn, number)];
-    var count = 0;
-    var results = validationResults.reduce(function (acc, r) {
-      var keys = Object.keys(r);
-      var currKey = keys[keys.length - 1];
-      if (!r.isValid) {
-        count++;
-      }
-      acc[currKey] = r[currKey];
-      return acc;
-    }, {});
-    results.isValid = count === 0;
-    return results;
-  };
   var simpleCard = function simpleCard(card) {
-    if (Object.prototype.toString.call(card) !== '[object Object]') {
+    if (type(card) !== 'Object') {
       throw new TypeError('Must send full card object to run full validation');
     }
-    return validation(card);
+    var validationResults = [cNumber(card.number), validCVN(card.cvn), expired(card.date), validMatch(card.cvn, card.number)];
+    return assign.apply(void 0, [{}].concat(validationResults, [{
+      isValid: every(function (x) {
+        return x.isValid;
+      }, validationResults)
+    }]));
   };
 
   exports.cvn = validCVN;
