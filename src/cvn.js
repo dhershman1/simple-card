2fbd257e14770d156120ce11b5c45b182531d638
dhershman1 <dustinh17@gmail.com>
2018-09-05 23:13:36 -0400

    Rebuilt cvn validation

--- src/cvn.js
+++ src/cvn.js
@@ -1,19 +1,5 @@
 import typeCheck from './_internals/typeCheck'
 
-const getCvnType = cvn => {
-  const len = cvn.length
-
-  if (len === 3) {
-    return 'norm'
-  }
-
-  if (len === 4) {
-    return 'amex'
-  }
-
-  return false
-}
-
 /**
  * @name cvn
  * @since v2.0.0
@@ -24,32 +10,29 @@ const getCvnType = cvn => {
  * @return {Object} An object containing a isValid boolean and some info
  *
  * @example
- * cvn('333'); // => { isValid: true, cvnType: 'norm' }
- * cvn(333); // => { isValid: true, cvnType: 'norm' }
- * cvn('4444'); // => { isValid: true, cvnType: 'amex' }
- * cvn(4444); // => { isValid: true, cvnType: 'amex' }
- * cvn('55555'); // => { isValid: false, cvnType: 'Invalid CVN Code' }
- * cvn(55555); // => { isValid: false, cvnType: 'Invalid CVN Code' }
+ * cvn('333') // => { isValid: true, cvnType: 'norm' }
+ * cvn(333) // => { isValid: true, cvnType: 'norm' }
+ * cvn('4444') // => { isValid: true, cvnType: 'amex' }
+ * cvn(4444) // => { isValid: true, cvnType: 'amex' }
+ * cvn('55555') // => { isValid: false, cvnType: 'Invalid CVN Code' }
+ * cvn(55555) // => { isValid: false, cvnType: 'Invalid CVN Code' }
  */
 export default cvnCode => {
   if (!typeCheck(cvnCode)) {
     throw new TypeError('cvn should be a string or number type')
   }
 
-  const results = {
-    isValid: false,
-    cvnType: 'Invalid CVN Code'
-  }
-  const sanitizedCVN = String(cvnCode).replace(/\D/g, '')
+  const sanitized = String(cvnCode).replace(/\D/g, '')
+  let type = 'Invalid CVN Code'
 
-  const type = getCvnType(sanitizedCVN)
-
-  if (type) {
-    return {
-      isValid: true,
-      cvnType: type
-    }
+  if (sanitized.length === 3) {
+    type = 'norm'
+  } else if (sanitized.length === 4) {
+    type = 'amex'
   }
 
-  return results
+  return {
+    isValid: type !== 'Invalid CVN Code',
+    cvnType: type
+  }
 }
